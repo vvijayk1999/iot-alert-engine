ae1d3a74a1e445195c865f6265445e77b256209c
Vijay Krishnan V <vvijayk1999@Vijays-MacBook-Air.local>
2025-07-14 11:51:37 +0530

    project setup

---
+++ src/main/java/io/github/vvijayk1999/iot_alert_engine/IoTAlertEngineApplication.java
@@ -0,0 +1,13 @@
+package io.github.vvijayk1999.iot_alert_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTAlertEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTAlertEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vvijayk1999/iot_alert_engine/IoTAlertEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vvijayk1999.iot_alert_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTAlertEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
